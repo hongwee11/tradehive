732ccddc210b8263cc757315de094b29fea489ca
Hongwee11 <hongwee11@gmail.com>
2025-06-15 15:39:11 +0800

    linking everything

--- dashboard-react/src/App.jsx
+++ dashboard-react/src/App.jsx
@@ -3,7 +3,9 @@ import LoginPage from './loginpage/LoginPage';
 import Dashboard from './dashboard/dashboard';
 import TradeForm from './TradeForm';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import PostPage from './forum/PostPage';
+import ForumHome from './forum/ForumHome';
+import CreatePost from './forum/CreatePost';
 
 function App() {
   return (
@@ -11,7 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/trade" element={<TradeForm />} /> 
+        <Route path="/trade" element={<TradeForm />} />
+        <Route path="/forum" element={<ForumHome />} />
+        <Route path="/forum/post/:id" element={<PostPage />} />
+        <Route path="/forum/create" element={<CreatePost />} />
       </Routes>
     </Router>
   );
